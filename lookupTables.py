368eb548e627fde57ff3ff8539f694f052a7eaf2
Nicholas Cueto <nicholascueto@Nicholass-MBP.lan>
2021-08-03 20:56:54 -0600

    Searc and replace working. Need to create tables now. *sigh*

---
+++ lookupTables.py
@@ -0,0 +1,6 @@
+# REGEX : [Replacement Name, Cat, SubCat]
+# Match Name and convert for database
+
+payees = {
+"SPROUTS FARMERS MKT|sprouts" : ["Sprouts Farmer's Market", "Groceries", ""],
+}
--- main.py
+++ main.py
@@ -1,11 +1,57 @@
 import os, sys
+import lookupTables as lut
 from fuzzywuzzy import fuzz
 
-with open ("CheckingExport.csv", "r") as f:
-    lines = f.readlines()
-    for i, x in enumerate(lines):
-        splitLine = x.split(',')
-        splitLine.pop(-1)
-        print(splitLine)
-        if i == 2:
+def searchAndReplace(line, table, oneLine=True, lineIndex=3, matchTolerance=80):
+    '''
+    Input list from csv and search through it using all search terms from 'lookupTables.py'.
+
+    line : list or str
+        Line from csv converted to list.
+
+    table : dic
+        Chosen Lookup Table. Dictionary formatted as Search Term : Replacement List.
+            Example: {"sprouts" : ["Sprouts Farmer's Market", "Groceries",,]}
+
+    oneLine : bool
+        If True, will process only one item list input. Default for this usecase is 3.
+
+    lineIndex : int
+        Index of item in list to prcess.
+    '''
+
+    keyValues = table.items()
+
+    for item in line:
+        if oneLine:
+            item = line[lineIndex]
+
+        for k, v in keyValues:
+            # input(k)
+            # input(v)
+            searchTerm, replacement = k, v
+            matchStrength = fuzz.token_set_ratio(searchTerm, item)
+
+            if matchStrength > matchTolerance:
+                print(f'Matched {searchTerm} and {item} with a match confidence of {matchStrength}. \n    Replacing with {replacement}')
+                break
+
+        if oneLine:
             break
+
+# open file
+with open ("Export.csv", "r") as f:
+    lines = f.readlines()
+
+for i, line in enumerate(lines):
+    splitLine = line.split(',')
+    # remove last line
+    splitLine.pop(-1)
+    # print(splitLine)
+
+    # Search through third item in list.
+    searchAndReplace(splitLine, lut.payees)
+    # if i == 30:
+    #     break
+
+
